453c89afa87e7a3db3eb15e85ed5c28d2e2c526b
Khomsan Chooon <85799327+MarkKh@users.noreply.github.com>
2023-07-29 01:51:47 +0700

    Create CoopCSVUploader.jsx

---
+++ client/src/admin/components/Coop/CoopCSVUploader.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Swal from 'sweetalert2';
+import SectionTitle from '../../components/Typography/SectionTitle';
+import { Button, Input } from "@windmill/react-ui";
+
+function CSVUploader({ fetchData }) {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleCSVUpload = () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post('http://localhost:3000/api/upload', formData)
+      .then((response) => {
+        console.log('Data uploaded successfully');
+        Swal.fire({
+          icon: 'success',
+          title: 'Success',
+          text: 'Data uploaded successfully',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "/admin/coop";
+            fetchData();
+          }
+        })
+      })
+      .catch((error) => {
+        console.error('Error uploading data:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'An error occurred while uploading data',
+        });
+      });
+  };
+
+  return (
+    <div>
+      <SectionTitle>Add Cooparative Data By CSV</SectionTitle>
+
+      <div className="px-4 py-6 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex space-x-2">
+
+          <Input
+            type="file"
+            accept=".csv"
+            onChange={handleFileChange}
+            required
+          />
+          <Button onClick={handleCSVUpload}>Upload</Button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default CSVUploader;
