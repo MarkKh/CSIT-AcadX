cefdb980bb98c24b8dc2b07f14bf06da4ba85bc5
Khomsan Chooon <85799327+MarkKh@users.noreply.github.com>
2023-07-28 14:41:07 +0700

    Update CSVUploader.jsx

--- client/src/admin/components/Report/CSVUploader.jsx
+++ client/src/admin/components/Report/CSVUploader.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Swal from 'sweetalert2';
+import SectionTitle from '../../components/Typography/SectionTitle';
+import { Button, Input } from "@windmill/react-ui";
+
 
 function CSVUploader({ fetchData }) {
   const [file, setFile] = useState(null);
@@ -38,8 +41,20 @@ function CSVUploader({ fetchData }) {
 
   return (
     <div>
-      <input type="file" accept=".csv" onChange={handleFileChange} />
-      <button onClick={handleCSVUpload}>Upload CSV</button>
+      <SectionTitle>Add Report Data By CSV</SectionTitle>
+
+      <div className="px-4 py-6 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex space-x-2">
+
+          <Input
+            type="file"
+            accept=".csv"
+            onChange={handleFileChange}
+            />
+          <Button onClick={handleCSVUpload}>Upload</Button>
+        </div>
+
+      </div>
     </div>
   );
 }
