43c6c732245dedec4443d6327dbacfc2dc70c4f8
Khomsan Chooon <85799327+MarkKh@users.noreply.github.com>
2023-07-29 01:52:01 +0700

    Update ReportSearch.jsx

--- client/src/admin/components/Report/ReportSearch.jsx
+++ client/src/admin/components/Report/ReportSearch.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { Input, Button } from "@windmill/react-ui";
 
-function ReportSearch({ setSearch }) {
+function ReportSearch({search, setSearch }) {
   return (
     <div className="flex justify-between mb-4">
       <div className="relative flex-1 mr-4">
         <Input
           type="text"
           onChange={(e) => setSearch(e.target.value)}
+          value={search}
           placeholder="Search..."
           className="border border-gray-300 p-2 rounded-md focus:outline-none w-full"
         />
