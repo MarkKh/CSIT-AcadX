b124ea369598cf6f5418a2b7ae04d5cc4feaa08b
Khomsan Chooon <85799327+MarkKh@users.noreply.github.com>
2023-07-29 22:10:48 +0700

    Create AdvisorDataToExcel.jsx

---
+++ client/src/admin/components/Advisor/AdvisorDataToExcel.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
+import {
+    Button,
+} from "@windmill/react-ui";
+
+
+const ReportDataToExcel = ({ dataToExcel }) => {
+    const handleExportExcel = () => {
+        // Create a new Workbook
+        const workbook = XLSX.utils.book_new();
+
+        // Create a new Worksheet
+        const worksheet = XLSX.utils.json_to_sheet(dataToExcel);
+
+        // Add the Worksheet to the Workbook
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Advisor Data");
+
+        // Create an Excel buffer
+        const excelBuffer = XLSX.write(workbook, { type: "array" });
+
+        // Create a Blob from the buffer
+        const blob = new Blob([excelBuffer], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+
+        // Download the Excel file
+        saveAs(blob, "Advisor data.xlsx");
+    };
+
+    return (
+        <div className="py-5">
+            <div className="flex justify-center">
+                <Button layout="outline" onClick={handleExportExcel}>Export to Excel</Button>
+            </div>
+        </div>
+
+
+    )
+};
+
+export default ReportDataToExcel;
